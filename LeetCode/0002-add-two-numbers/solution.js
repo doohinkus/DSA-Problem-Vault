1ee5af7b1d3991f576956b4acf1f30152ad64a46
Rafael Is Coding <clickpass@gmail.com>
2026-08-26 18:00:46 -0700

    Sync LeetCode: Add Two Numbers (AI: O(N))

--- LeetCode/0002-add-two-numbers/solution.js
+++ LeetCode/0002-add-two-numbers/solution.js
@@ -1,11 +1,6 @@
+
+
     while (l1 || l2 || carry) {
-
-    let carry = 0;
-    let tail = dummy;
-    const dummy = { val: 0, next: null };
-var addTwoNumbers = function(l1, l2) {
-
- */
         const sum = (l1 ? l1.val : 0) +
                     (l2 ? l2.val : 0) +
                     carry;
@@ -18,3 +13,9 @@ var addTwoNumbers = function(l1, l2) {
         };
 
         tail = tail.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    return holder.next;
