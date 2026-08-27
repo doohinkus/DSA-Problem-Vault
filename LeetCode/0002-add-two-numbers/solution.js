b7b0e94f03f064ca9a37767af5490a464007a083
Rafael Is Coding <clickpass@gmail.com>
2026-08-26 18:41:54 -0700

    Sync LeetCode: Add Two Numbers (AI: O(N))

--- LeetCode/0002-add-two-numbers/solution.js
+++ LeetCode/0002-add-two-numbers/solution.js
@@ -1,4 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
 
+var addTwoNumbers = function(l1, l2) {
+    const holder = { val: 0, next: null };
+    let tail = holder;
+    let carry = 0;
 
     while (l1 || l2 || carry) {
         const sum = (l1 ? l1.val : 0) +
@@ -19,3 +35,5 @@
     }
 
     return holder.next;
+
+};
