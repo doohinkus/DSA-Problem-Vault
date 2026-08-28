45bc5a9f620e7a9ea6e6e002354cedf95b0c056e
Rafael Is Coding <clickpass@gmail.com>
2026-08-28 11:10:20 -0700

    Sync LeetCode: Longest Substring Without Repeating Characters (AI: O(N²))

--- LeetCode/0003-longest-substring-without-repeating-characters/solution.js
+++ LeetCode/0003-longest-substring-without-repeating-characters/solution.js
@@ -1,3 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+   let uniqueChars = new Set();
+   let start = 0, end = 0, maxLength = 0;
+
+   while(end < s.length){
+    while(uniqueChars.has(s[end])){
+        uniqueChars.delete(s[start]);
+        start++;
+    }
+    uniqueChars.add(s[end]);
+    maxLength = Math.max(maxLength, end - start + 1);
     end++;
    }
 
