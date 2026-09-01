ea5dcde38cb96a68380e109530e66061990cbb11
Rafael Is Coding <clickpass@gmail.com>
2026-09-01 09:18:35 -0700

    Sync LeetCode: Car Fleet (AI: O(N log N))

--- LeetCode/0853-car-fleet/solution.js
+++ LeetCode/0853-car-fleet/solution.js
@@ -5,19 +5,26 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
+    // pair cars with their position
     const cars = position.map((pos, i) => [pos, speed[i]]);
+    //sort cars from closets to furthest from target
     cars.sort((a, b) => b[0] - a[0]);
     let fleets = 0;
     let fleetTime = 0;
 
     for(const [pos, spd] of cars){
+        // Time required for car to reach target
         const time =  (target - pos) / spd;
+        // When time to reach target is longer than fleetTime
+        // The car cannot catch the fleet ahead
+        // Add to fleet
+        // update fleetTime
         if(time > fleetTime){
           fleets++;
           fleetTime = time;
         }
     }
-    
+    // return the number of fleets
     return fleets;
     
 };
