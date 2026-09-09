09932609d8c303132e49471b68d0a5c3a8b3b2e7
Rafael Is Coding <clickpass@gmail.com>
2026-09-08 17:08:14 -0700

    Sync LeetCode: Intersection of Two Arrays (AI: O(N))

--- LeetCode/0349-intersection-of-two-arrays/solution.js
+++ LeetCode/0349-intersection-of-two-arrays/solution.js
@@ -4,14 +4,20 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let i = 0;
+    // Store all values from nums2 for fast lookup
+    const nums2Set = new Set(nums2);
+
+    // A Set automatically removes duplicate values
     const result = new Set();
-    while(i < nums1.length){
-      if(nums2.includes(nums1[i])){
-        result.add(nums1[i]);
-      }
-      i++;
+
+    for (const num of nums1) {
+        // Check whether nums2 contains this number
+        if (nums2Set.has(num)) {
+            result.add(num);
+        }
     }
+
+    // Convert the Set back into an array
     return [...result];
     
 };
