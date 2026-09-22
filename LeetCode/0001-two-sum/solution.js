2d784264f368888e7c5f05cea7b0d52a4415e794
Rafael Is Coding <clickpass@gmail.com>
2026-09-22 14:22:05 -0700

    Sync LeetCode: Two Sum (AI: O(N))

--- LeetCode/0001-two-sum/solution.js
+++ LeetCode/0001-two-sum/solution.js
@@ -4,18 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  let start = 0;
+   let start = 0;
   let end = nums.length;
   const complimentMap = new Map();
   
   while(start < end){
     const compliment = target - nums[start];
-    const complimentIndex = nums.indexOf(compliment);
                              
     if (complimentMap.has(compliment)){
       return [start, complimentMap.get(compliment)];
     }
-    // add nums to set--preserving index
     complimentMap.set(nums[start], start);
     start++;
   }
